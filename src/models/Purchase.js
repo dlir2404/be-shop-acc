72098d55d959eaa2213e861d8bce5fc1d3d9c3ea
dlir2404 <127393547+dlir2404@users.noreply.github.com>
2023-12-20 10:06:37 +0700

    fix

--- src/models/Purchase.js
+++ src/models/Purchase.js
@@ -1,10 +1,19 @@
 module.exports = (sequelize, DataTypes) => {
     const Purchase = sequelize.define('purchase', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
         userId: {
             type: DataTypes.INTEGER,
         },
         accountId: {
             type: DataTypes.INTEGER,
+        },
+        accountPrice: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
         },
         status: {
             type: DataTypes.STRING,
